2eb0ea5c903d42391d3a9b65faaa463859c8e9d5
Stas Lisetsky <stas.lisetsky@gmail.com>
2016-07-06 20:58:09 +0200

    Fix addon totals on product page

--- woocommerce/templates/single-product/add-to-cart/simple.php
+++ woocommerce/templates/single-product/add-to-cart/simple.php
@@ -32,13 +32,28 @@ if ( ! $product->is_purchasable() ) {
 	// Availability
 	$availability      = $product->get_availability();
 	$availability_html = empty( $availability['availability'] ) ? '' : '<p class="stock ' . esc_attr( $availability['class'] ) . '">' . esc_html( $availability['availability'] ) . '</p>';
-
 	echo apply_filters( 'woocommerce_stock_html', $availability_html, $availability['availability'], $product );
+	
+	$pricing_groups = get_post_meta( $product->post->ID, '_pricing_rules', true );
 ?>
 
 <?php if ( $product->is_in_stock() ) : ?>
 
 	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+	<script type="text/javascript"> 
+		// Note(stas):
+		// Load dynamic pricing into a Json array
+		// for later use form Product Add-ons JS to calculate grand totals.
+		// discountRules array will be used as a global.
+
+		var discountRules = new Array();
+	<?php $i=0; ?>
+	<?php if ($pricing_groups): ?>
+	<?php foreach($pricing_groups as $key => $group): ?>discountRules[<?php echo $i; ?>] = <?php echo json_encode($group); ?>;<?php endforeach ?>
+	<?php endif ?>
+	</script>
+	<?php $i++; ?>
+
 
 	<form class="cart" method="post" enctype='multipart/form-data'>
 	 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
